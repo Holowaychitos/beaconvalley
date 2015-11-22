faf087279242ee4474af6db90418b3eb5e36ae8d
mikejauregui <m.jauregui17@gmail.com>
2015-11-22 04:30:07 -0600

    Places added to "Shopping"
    Added 5 new places to the Shopping Quest

--- src/hardcoded/places.js
+++ src/hardcoded/places.js
@@ -4,19 +4,6 @@ module.exports = {
     subtitle: 'La lupita, la chiquirruquis...',
     code: 'NIGHT',
     imageUrl: 'https://d7cc6r9c9cz1k.cloudfront.net/original/original_56516996e5e3f7a662aa72d2.jpg',
-    places: [{
-      id: '1',
-      latitude: 1,
-      longitude: 2,
-      animateDrop: true,
-      title: 'Nombre'
-    }, {
-      id: '2',
-      latitude: 3,
-      longitude: 4,
-      animateDrop: true,
-      title: 'Nombre'
-    }]
   },
   HISTORY: {
     name: 'Centro histórico',
@@ -29,6 +16,42 @@ module.exports = {
     subtitle: 'Aprende algo dinero',
     code: 'SHOPPING',
     imageUrl: 'https://d7cc6r9c9cz1k.cloudfront.net/original/original_5651697be5e3f7a662aa72d1.jpg'
+    places: [{
+      id: '1',
+      latitude: 20.710348,
+      longitude: -103.412186,
+      animateDrop: true,
+      title: 'Andares'
+    },
+    {
+      id: '2',
+      latitude: 20.711993,
+      longitude: -103.411028,
+      animateDrop: true,
+      title: 'Cinepolis'
+    },
+    {
+      id: '3',
+      latitude: 20.710368,
+      longitude: -103.411822,
+      animateDrop: true,
+      title: 'Palacio de Hierro'
+    },
+    {
+      id: '4',
+      latitude: 20.709374,
+      longitude: -103.410899,
+      animateDrop: true,
+      title: 'Superama'
+    },
+    {
+      id: '5',
+      latitude: 20.712465,
+      longitude: -103.411972,
+      animateDrop: true,
+      title: 'Liverpool'
+    }]
+    
   },
   TEQUILA: {
     name: 'Tequila Mágico',
